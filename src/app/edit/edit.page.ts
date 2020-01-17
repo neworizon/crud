df3b7189cdd3869a6c9cd74669dc252b9d06675f
P Suresh <psuresh@Ps-MacBook-Air.local>
2020-01-17 13:03:08 +0530

    changed update page

--- src/app/edit/edit.page.ts
+++ src/app/edit/edit.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.page.scss'],
 })
 export class EditPage implements OnInit {
+  baseUrl = 'http://localhost:3000/users'
+  id;
+  selectedEmployee :any={name:'',phone:'',id:''};
+  constructor(private route : ActivatedRoute, private http: HttpClient) { }
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    // console.log(this.id);
+    // console.log(`${this.baseUrl}/${this.id}`);
+    this.http.get(`${this.baseUrl}/${this.id}`).subscribe( res => {
+      this.selectedEmployee = res;
+      console.log(this.selectedEmployee);
+    })
+  }
+
+
+  update() {
+    let updatedData ={
+      id: this.selectedEmployee.id,
+      name: this.selectedEmployee.name,
+      phone: this.selectedEmployee.phone
+    }
+    // data = JSON.stringify(this.new)
+    this.http.put(`${this.baseUrl}/${this.id}`, JSON.stringify(updatedData), this.httpOptions).subscribe(res => console.log(res));
+    
   }
 
 }
